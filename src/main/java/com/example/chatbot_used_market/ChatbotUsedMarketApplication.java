9298e1f2a8093bd114b831c62fc6b1266f79c383
renetdevp <renetdevp@gmail.com>
2025-07-08 10:26:49 +0900

    Spring project initialization

---
+++ src/main/java/com/example/chatbot_used_market/ChatbotUsedMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatbot_used_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotUsedMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotUsedMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatbot_used_market/ChatbotUsedMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbot_used_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotUsedMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
