c24347bed61dc16565f23ea3802646b87ff6323e
renetdevp <renetdevp@gmail.com>
2025-07-17 13:10:41 +0900

    FEAT: Write api_request logger method(Not aplied), Remove unnecessary dependency of googleGeocodingClient method

--- src/main/java/com/example/chatbot_used_market/config/WebClientConfig.java
+++ src/main/java/com/example/chatbot_used_market/config/WebClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Collections;
@@ -18,13 +19,27 @@ public class WebClientConfig {
   private String googleGeocodingAPIKey;
 
   @Bean
-  public WebClient googleGeocodingClient(WebClient.Builder builder){
-    return builder
+  public WebClient googleGeocodingClient(){
+    return WebClient.builder()
             .baseUrl(googleGeocodingAPIUrl)
             .defaultUriVariables(Collections.singletonMap("apiKey", googleGeocodingAPIKey))
             .defaultHeaders(httpHeaders -> {
               httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
             })
             .build();
+  }
+
+  // logging when request to google geocoding request
+  public static ExchangeFilterFunction logRequest() {
+    return (clientRequest, next) -> {
+//      log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
+      System.out.printf("Request: %s %s \n", clientRequest.method(), clientRequest.url());
+//      clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
+      clientRequest.headers().forEach((name, values) -> values.forEach(value ->
+//              log.info("{}={}", name, value)
+                      System.out.printf("%s=%s \n", name, value)
+      ));
+      return next.exchange(clientRequest);
+    };
   }
 }
