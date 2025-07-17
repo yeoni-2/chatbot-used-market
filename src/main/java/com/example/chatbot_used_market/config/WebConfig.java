9198fc7be667f409fb48d3a9f7f203e0ac711b7f
bangjjun <p01085488742@gmail.com>
2025-07-17 11:19:28 +0900

    feature/login (#13)
    * FEAT: add data to store in the session
    
    * build: interceptor
    
    * feat: add main page button to header.html
    
    * fix: change css in header.html, remove CSRF
    
    * feat: nickname exception handling when OAuth2 login
    
    * feat: exception handling when OAuth2 login using the same email
    
    * feat: add AuthProvider

---
+++ src/main/java/com/example/chatbot_used_market/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.chatbot_used_market.config;
+
+import com.example.chatbot_used_market.interceptor.LoginUserInterceptor;
+import com.example.chatbot_used_market.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final UserService userService;
+
+    @Autowired
+    public WebConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginUserInterceptor(userService))
+                .addPathPatterns("/trade", "/trades/**", "/mypage/**"); // 로그인 후 접근 가능한 경로만 지정
+    }
+}
