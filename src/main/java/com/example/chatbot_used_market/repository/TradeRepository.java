fbee47b8144f5079a7e6504ebc8245c6c6b80de2
bangjjun <p01085488742@gmail.com>
2025-07-21 01:09:02 +0900

    feat: add infinite scroll

--- src/main/java/com/example/chatbot_used_market/repository/TradeRepository.java
+++ src/main/java/com/example/chatbot_used_market/repository/TradeRepository.java
@@ -3,10 +3,13 @@ package com.example.chatbot_used_market.repository;
 import com.example.chatbot_used_market.entity.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 @Repository
 public interface TradeRepository extends JpaRepository<Trade, Long> {
     List<Trade> findByStatusOrderByViewCountDesc(String status);
+    Page<Trade> findByStatus(String status, Pageable pageable);
 }
