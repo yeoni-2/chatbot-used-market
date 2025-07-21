fe2060b6365f1c99133f889dac3f8ea631778c6d
yeoni-2 <kjywjddus1014@gmail.com>
2025-07-21 14:15:22 +0900

    Feature/trade search order (#20)
    * FEAT: search feature
    
    * FEAT: search.html
    
    * STYLE: fix header.html, search.html
    
    * FEAT: category search feature, add TradeImage entity
    
    * FEAT: category search feature, add TradeImage entity
    
    * FIX: hide application-postgresql.properties
    
    * stop tracking application-postgresql.properties
    
    * FEAT: add upload image to AWS S3 and style trade.html, search.html, trade_post.html, add pagenation
    
    * FIX: issue where posts could not be deleted after creating a chat room

---
+++ src/main/java/com/example/chatbot_used_market/entity/TradeImage.java
@@ -0,0 +1,24 @@
+package com.example.chatbot_used_market.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "trade_images")
+@Getter
+@Setter
+@NoArgsConstructor
+public class TradeImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trade_id", nullable = false)
+    private Trade trade;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+} 
