77299d6093cc5558bbae1763d66d9e0adc0a4abb
bangjjun <p01085488742@gmail.com>
2025-07-22 13:39:37 +0900

    Revert "feature/main-style (#30)" (#31)
    This reverts commit ed6cc1c0d3cb440660d3003d020b484ce80b5081.

--- src/main/java/com/example/chatbot_used_market/ChatbotUsedMarketApplication.java
+++ src/main/java/com/example/chatbot_used_market/ChatbotUsedMarketApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatbotUsedMarketApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotUsedMarketApplication.class, args);
 	}
+
 }
